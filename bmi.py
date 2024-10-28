99f1da12e6764efc673437093a99630dbca14508
AungLinHtet <aunglin.23@ichat.sp.edu.sg>
2024-10-28 09:42:38 +0800

    After fixing

--- bmi.py
+++ bmi.py
@@ -5,9 +5,12 @@ def calculate_bmi(height,weight):
     print(f"Your BMI is {bmi:.2f}")
     if bmi<18.5:
         classidentification= "Under weight"
+        return -1
     elif 18.5 <= bmi <= 25.0:
         classidentification= "Normal Weight"
+        return 0
     else:
         classidentification= "Over Weight"
+        return 1
     print(f"Weight Classification: {classidentification}")
 calculate_bmi(weight=57,height=1.73)    
